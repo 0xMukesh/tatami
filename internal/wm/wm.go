d202b9efdaf4be59a89db968fcf3388d2af054a8
Mukesh <hey.mukesh272921@gmail.com>
2026-03-16 00:44:08 +0530

    feat: titlebars

--- internal/wm/wm.go
+++ internal/wm/wm.go
@@ -17,21 +17,19 @@ type Wm struct {
 	setup      *xproto.SetupInfo
 	screen     *xproto.ScreenInfo
 	rootWindow xproto.Window
-
-	// mappings
-	clients map[xproto.Window]xproto.Window
-	atoms   map[string]xproto.Atom
-
-	// configuration
-	mod      uint16
-	launcher string
-	display  string
+	titleBarGc xproto.Gcontext
+	clients    map[xproto.Window]xproto.Window
+	titleBars  map[xproto.Window]xproto.Window
+	atoms      map[string]xproto.Atom
+	mod        uint16
+	launcher   string
+	display    string
 }
 
 func New(wmConfig config.WmConfig) (*Wm, error) {
 	conn, err := xgb.NewConn()
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect to x server. error - %w", err)
+		return nil, fmt.Errorf("failed to connect to x server - %w", err)
 	}
 
 	setup := xproto.Setup(conn)
@@ -46,6 +44,7 @@ func New(wmConfig config.WmConfig) (*Wm, error) {
 		rootWindow: root,
 		clients:    make(map[xproto.Window]xproto.Window),
 		atoms:      make(map[string]xproto.Atom),
+		titleBars:  make(map[xproto.Window]xproto.Window),
 		mod:        wmConfig.ModifierMask,
 		launcher:   wmConfig.Launcher,
 		display:    display,
@@ -68,10 +67,28 @@ func New(wmConfig config.WmConfig) (*Wm, error) {
 			return nil, fmt.Errorf("could not become wm. is another instance of wm already running?")
 		}
 
-		return nil, fmt.Errorf("could not become wm. error - %w", err)
+		return nil, fmt.Errorf("could not become wm - %w", err)
 	}
 
-	wm.getAndCacheAtoms([]string{constants.ATOM_WM_PROTOCOLS, constants.ATOM_WM_DELETE_WINDOW})
+	// creating a graphical context for title bar at root window level and caching it
+	titleBarGc, err := xproto.NewGcontextId(wm.conn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create title bar graphical context ID - %w", err)
+	}
+
+	if err := xproto.CreateGCChecked(
+		wm.conn, titleBarGc, xproto.Drawable(root),
+		xproto.GcForeground|xproto.GcBackground,
+		[]uint32{constants.TITLE_BAR_FG, constants.TITLE_BAR_BG},
+	).Check(); err != nil {
+		return nil, fmt.Errorf("failed to store title bar graphical context - %w", err)
+	}
+
+	wm.titleBarGc = titleBarGc
+
+	// caching commonly used atoms
+	wm.getAndCacheAtoms([]string{constants.ATOM_WM_PROTOCOLS, constants.ATOM_WM_DELETE_WINDOW, constants.ATOM_NET_WM_NAME, constants.ATOM_TYPE_UTF8_STRING})
+	// registering keyboard shortcuts for root window
 	wm.registerShortcuts(wm.rootWindow)
 	return wm, nil
 }
@@ -91,6 +108,8 @@ func (wm *Wm) Run() {
 		switch v := ev.(type) {
 		case xproto.KeyPressEvent:
 			wm.handleKeyPressEvent(v)
+		case xproto.ExposeEvent:
+			wm.handleExposeEvent(v)
 		case xproto.ConfigureRequestEvent:
 			wm.handleConfigureRequest(v)
 		case xproto.MapRequestEvent:
@@ -101,14 +120,14 @@ func (wm *Wm) Run() {
 			wm.handleDestroyNotify(v)
 		}
 	}
-
 }
 
 func (wm *Wm) Close() {
+	xproto.FreeGC(wm.conn, wm.titleBarGc)
 	wm.conn.Close()
 }
 
-// event handlers
+// core handlers
 func (wm *Wm) handleKeyPressEvent(v xproto.KeyPressEvent) {
 	keycode := int(v.Detail)
 	mod := v.State
@@ -123,6 +142,20 @@ func (wm *Wm) handleKeyPressEvent(v xproto.KeyPressEvent) {
 		for child, frame := range wm.clients {
 			if frame == v.Child {
 				wm.closeWindow(child)
+			}
+		}
+	}
+}
+
+func (wm *Wm) handleExposeEvent(v xproto.ExposeEvent) {
+	// render title bars
+	for child, tb := range wm.titleBars {
+		if tb == v.Window {
+			title := wm.getWindowTitle(child)
+
+			if err := xproto.ImageText8Checked(wm.conn, byte(len(title)), xproto.Drawable(tb), wm.titleBarGc, 8, 16, title).Check(); err != nil {
+				slog.Error("failed to render title text", slog.String("error", err.Error()))
+				return
 			}
 		}
 	}
@@ -176,7 +209,12 @@ func (wm *Wm) handleMapRequest(v xproto.MapRequestEvent) {
 		wm.conn,
 		child,
 		xproto.ConfigWindowX|xproto.ConfigWindowY|xproto.ConfigWindowWidth|xproto.ConfigWindowHeight,
-		[]uint32{0, 0, uint32(wm.screen.WidthInPixels), uint32(wm.screen.HeightInPixels)},
+		[]uint32{
+			0,
+			constants.TITLE_BAR_HEIGHT,
+			uint32(wm.screen.WidthInPixels),
+			uint32(wm.screen.HeightInPixels) - uint32(constants.TITLE_BAR_HEIGHT),
+		},
 	).Check(); err != nil {
 		slog.Error("failed to configure child window", slog.String("error", err.Error()))
 		return
@@ -196,7 +234,7 @@ func (wm *Wm) handleMapRequest(v xproto.MapRequestEvent) {
 		wm.screen.RootVisual,
 		xproto.CwBackPixel|xproto.CwEventMask,
 		[]uint32{
-			constants.DEFAULT_BACKGROUND,
+			constants.DEFAULT_BG,
 			xproto.EventMaskSubstructureRedirect | xproto.EventMaskSubstructureNotify,
 		},
 	).Check(); err != nil {
@@ -204,7 +242,9 @@ func (wm *Wm) handleMapRequest(v xproto.MapRequestEvent) {
 		return
 	}
 
-	if err := xproto.ReparentWindowChecked(wm.conn, v.Window, frame, 0, 0).Check(); err != nil {
+	titleBar := wm.createTitleBarWindow(frame)
+
+	if err := xproto.ReparentWindowChecked(wm.conn, v.Window, frame, 0, constants.TITLE_BAR_HEIGHT).Check(); err != nil {
 		slog.Error("failed to reparent windows", slog.String("error", err.Error()))
 		return
 	}
@@ -221,6 +261,11 @@ func (wm *Wm) handleMapRequest(v xproto.MapRequestEvent) {
 			return
 		}
 
+		if err := xproto.MapWindowChecked(wm.conn, titleBar).Check(); err != nil {
+			slog.Error("failed to map title bar window", slog.String("error", err.Error()))
+			return
+		}
+
 		if err := xproto.MapWindowChecked(wm.conn, v.Window).Check(); err != nil {
 			slog.Error("failed to map child window", slog.String("error", err.Error()))
 			return
@@ -228,6 +273,7 @@ func (wm *Wm) handleMapRequest(v xproto.MapRequestEvent) {
 
 		wm.registerShortcuts(frame)
 		wm.clients[child] = frame
+		wm.titleBars[child] = titleBar
 	}
 }
 
@@ -274,6 +320,7 @@ func (wm *Wm) removeWindow(window xproto.Window) {
 	}
 
 	delete(wm.clients, window)
+	delete(wm.titleBars, window)
 }
 
 func (wm *Wm) closeWindow(window xproto.Window) {
@@ -291,6 +338,37 @@ func (wm *Wm) closeWindow(window xproto.Window) {
 	} else {
 		xproto.DestroyWindow(wm.conn, window)
 	}
+}
+
+func (wm *Wm) createTitleBarWindow(frameWindow xproto.Window) xproto.Window {
+	titleBar, err := xproto.NewWindowId(wm.conn)
+	if err != nil {
+		slog.Error("failed to create a titlebar window", slog.String("error", err.Error()))
+		return xproto.BadWindow
+	}
+
+	if err := xproto.CreateWindowChecked(
+		wm.conn,
+		wm.screen.RootDepth,
+		titleBar,
+		frameWindow,
+		0, 0,
+		wm.screen.WidthInPixels,
+		constants.TITLE_BAR_HEIGHT,
+		0,
+		xproto.WindowClassInputOutput,
+		wm.screen.RootVisual,
+		xproto.CwBackPixel|xproto.CwEventMask,
+		[]uint32{
+			constants.TITLE_BAR_BG,
+			xproto.EventMaskExposure,
+		},
+	).Check(); err != nil {
+		slog.Error("failed to create title bar window", slog.String("error", err.Error()))
+		return xproto.BadWindow
+	}
+
+	return titleBar
 }
 
 // utils
@@ -311,12 +389,12 @@ func (wm *Wm) getAtom(property string) (xproto.Atom, error) {
 
 func (wm *Wm) getAndCacheAtoms(properties []string) error {
 	for _, p := range properties {
-		atom, err := wm.getAtom(p)
+		reply, err := xproto.InternAtom(wm.conn, true, uint16(len(p)), p).Reply()
 		if err != nil {
 			return fmt.Errorf("failed to get atom for %v - %w", p, err)
 		}
 
-		wm.atoms[p] = atom
+		wm.atoms[p] = reply.Atom
 	}
 
 	return nil
@@ -352,4 +430,21 @@ func (wm *Wm) doesSupportDeleteProtocol(window xproto.Window) bool {
 	}
 
 	return false
+}
+
+func (wm *Wm) getWindowTitle(window xproto.Window) string {
+	atomNetWmName := wm.atoms[constants.ATOM_NET_WM_NAME]
+	atomTypeUtf8String := wm.atoms[constants.ATOM_TYPE_UTF8_STRING]
+
+	prop, err := xproto.GetProperty(wm.conn, false, window, atomNetWmName, atomTypeUtf8String, 0, 256).Reply()
+	if err == nil && prop.ValueLen > 0 {
+		return string(prop.Value)
+	}
+
+	prop, err = xproto.GetProperty(wm.conn, false, window, xproto.AtomWmName, xproto.AtomString, 0, 256).Reply()
+	if err == nil && prop.ValueLen > 0 {
+		return string(prop.Value)
+	}
+
+	return "Untitled"
 }
